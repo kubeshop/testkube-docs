17e17ee68f467c7c6e2ec43dbb81fa3b8b2ac6ed
Ole Lensmar <ole@lensmar.com>
2024-05-28 15:59:53 +0200

    removed redocusaurus for now..

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,21 +60,6 @@ const config = {
         },
       },
     ],
-    [
-      "redocusaurus",
-      {
-        // Plugin Options for loading OpenAPI files
-        specs: [
-          {
-            spec: "https://raw.githubusercontent.com/kubeshop/testkube/main/api/v1/testkube.yaml",
-            route: "/openapi",
-          },
-        ],
-        theme: {
-          primaryColor: "#818cf8",
-        },
-      },
-    ],
   ],
 
   themeConfig:
