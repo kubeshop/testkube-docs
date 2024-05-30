68f7072c6babb965cf99638779b71431f32a02f4
Ole Lensmar <ole@lensmar.com>
2024-05-30 08:29:51 +0200

    added comments..

--- src/scripts/split-openapi.ts
+++ src/scripts/split-openapi.ts
@@ -22,18 +22,19 @@ async function splitOpenAPIByPaths(url: string, outputDir: string): Promise<void
     const components = openapi.components;
     const redocSpecs: Object[] = [];
     const redocSidebar: Object[] = [];
+    const openapis = new Map<string, Map<string, any>>()
 
     if (!fs.existsSync(outputDir)) {
         fs.mkdirSync(outputDir, {recursive: true});
     }
 
+    // extract components to shared file
     if (components) {
         const componentsFile = path.join(outputDir, 'components.yaml');
         fs.writeFileSync(componentsFile, yaml.dump({components: components}, {sortKeys: false}));
     }
 
-    const openapis = new Map<string, Map<string, any>>()
-
+    // aggregate paths per root path segment
     for (const pathKey in paths) {
         const basePath = pathKey.substring(1).split('/')[0];
         if (!openapis.has(basePath)) {
@@ -43,10 +44,9 @@ async function splitOpenAPIByPaths(url: string, outputDir: string): Promise<void
         openapis.get(basePath).set(pathKey, paths[pathKey])
     }
 
+    // now create new openapi file for each root path segment
     openapis.forEach((paths, rootPath) => {
-
         const outputFile = path.join(outputDir, `${rootPath}.yaml`);
-
         const newOpenAPI: OpenAPI = {
             openapi: openapi.openapi || '3.0.0',
             info: openapi.info,
@@ -54,6 +54,7 @@ async function splitOpenAPIByPaths(url: string, outputDir: string): Promise<void
         };
 
         paths.forEach((value, key) => {
+            // delete tags to flatten redoc sidebar
             for (let key in value) {
                 delete value[key]?.tags
             }
@@ -61,20 +62,22 @@ async function splitOpenAPIByPaths(url: string, outputDir: string): Promise<void
             newOpenAPI.paths[key] = value;
         });
 
+        // generate YAML and replace component references
         let openApiYaml = yaml.dump(newOpenAPI, {sortKeys: false})
         openApiYaml = openApiYaml.replace(/#\/components/g, './components.yaml#/components');
 
         fs.writeFileSync(outputFile, openApiYaml);
         console.log("written " + outputFile + ", " + paths.size + " paths");
 
+        // add generated file to redoc config and sidebar
         redocSpecs.push({spec: outputFile, route: "openapi/testkube-" + rootPath});
         redocSidebar.push({type: "link", label: rootPath, href: "/openapi/testkube-" + rootPath});
     });
 
+    // write generated redoc specs and sidebar
     fs.writeFileSync(path.join(outputDir, "redoc-specs.js"), "module.exports = " + JSON.stringify(redocSpecs));
     fs.writeFileSync(path.join(outputDir, "redoc-sidebar.js"), "module.exports = " + JSON.stringify(redocSidebar));
 }
-
 
 const url = 'https://raw.githubusercontent.com/kubeshop/testkube/main/api/v1/testkube.yaml';
 const outputDir = 'src/openapi';
