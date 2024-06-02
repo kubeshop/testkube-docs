c22e1b230568b04db55a17aa400415683fa83e13
Julianne Fermi <julianne@kubeshop.io>
2024-06-02 10:01:53 -0500

    New sidebars.js
    Updated from the new outline for the docs.

--- sidebars.js
+++ sidebars.js
@@ -17,46 +17,55 @@ const sidebars = {
   tutorialSidebar: [
     {
       type: "category",
-      label: "Overview",
+      label: "Getting Started",
       link: {
         type: "doc",
-        id: "index",
+        id: "articles/getting-started",
       },
       items: [
-        "articles/supported-tests",
-        "articles/testkube-benefits",
-        "articles/open-source-or-pro",
-      ],
-    },
-    {
-      type: "doc",
-      label: "Getting Started",
-      id: "articles/getting-started",
+        {
+          type: "category",
+          label: "Welcome",
+          items: [
+            { type: "doc", id: "articles/supported-tests" },
+            { type: "doc", id: "articles/testkube-benefits" },
+            { type: "doc", id: "articles/open-source-or-pro" },
+          ],
+        },
+        {
+          type: "category",
+          label: "Installation",
+          items: [
+            { type: "doc", id: "articles/install/overview" },
+            { type: "doc", id: "articles/install/cli" },
+            { type: "doc", id: "articles/install/quickstart-install" },
+            { type: "doc", id: "articles/install/reference-architectures" },
+            { type: "doc", id: "articles/install/install-with-cli" },
+            { type: "doc", id: "articles/install/install-with-helm" },
+            { type: "doc", id: "articles/install/advanced-install" },
+            { type: "doc", id: "articles/install/quickstart-no-k8s" },
+            { type: "doc", id: "articles/install/uninstall" },
+            { type: "doc", id: "articles/install/faq" },
+          ],
+        },
+      ]
     },
     {
       type: "category",
       label: "Installing",
       items: [
-        { type: "doc", id: "articles/install/overview" },
-        { type: "doc", id: "articles/install/cli" },
-        { type: "doc", id: "articles/install/quickstart-install" },
-        { type: "doc", id: "articles/install/reference-architectures" },
-        { type: "doc", id: "articles/install/install-with-cli" },
-        { type: "doc", id: "articles/install/install-with-helm" },
-        { type: "doc", id: "articles/install/advanced-install" },
-        { type: "doc", id: "articles/install/quickstart-no-k8s" },
-        { type: "doc", id: "articles/install/uninstall" },
-        { type: "doc", id: "articles/install/faq" },
+        { type: "doc", label: "Deploying on AWS", id: "articles/deploying-in-aws" },
+        { type: "doc", label: "Deploying with Private Registries", id: "articles/deploying-from-private-registries" },
+      //  { type: "doc", label: "Using with Istio", id: "" },
+        { type: "doc", label: "Deploying in Air-gapped Environments", id: "articles/install/advanced-install" },
+      //  { type: "doc", label: "Auto-updating the Agent", id: "" },
+      //  { type: "doc", label: "Configuring S3 with AWS", id: "" },
       ],
     },
     {
       type: "category",
-      label: "Concepts",
+      label: "Core Concepts",
       items: [
-        {
-          type: "category",
-          label: "Test Workflows (beta)",
-          items: [
             "articles/test-workflows",
             "articles/test-workflows-creating",
             "articles/test-workflow-templates",
@@ -65,199 +74,223 @@ const sidebars = {
             "articles/test-workflows-examples-expressions",
             "articles/test-workflows-examples-templates",
           ],
-        },
-        {
-          type: "category",
-          label: "Tests",
-          items: [
-            "articles/creating-tests",
-            "articles/running-tests",
-            "articles/getting-tests-results",
-            "articles/adding-tests-variables",
-            "articles/adding-timeout",
-          ],
-        },
-        {
-          type: "category",
-          label: "Test Suites",
-          items: [
-            "articles/creating-test-suites",
-            "articles/running-test-suites",
-            "articles/getting-test-suites-results",
-          ],
-        },
-        {
-          type: "category",
-          label: "Testkube Dashboard",
+    },
+    {
+      type: "category",
+      label: "Using Testkube",
           link: {
             type: "doc",
             id: "articles/testkube-dashboard",
           },
           items: [
-            "articles/testkube-dashboard-explore",
-            "articles/testkube-dashboard-general-settings",
+            {
+              type: "category",
+              label: "Testkube Dashboard",
+              items: [
+                 { type: "doc", label: "Overview", id: "articles/testkube-dashboard-explore" },
+                 { type: "doc", label: "Settings", id: "articles/testkube-dashboard-general-settings" },
+              ],
+            },  
+            {
+              type: "category",
+              label: "Test Workflows",
+              items: [
+                 { type: "doc", label: "Overview", id: "articles/test-workflows" },
+               //  { type: "doc", label: "Defining", id: "" },
+               //  { type: "doc", label: "Parallelization", id: "" },
+               //  { type: "doc", label: "Sharding", id: "" },
+               //  { type: "doc", label: "Modularizing", id: "" },
+                 { type: "doc", label: "Paramterization", id: "articles/test-workflows-examples-configuration" },
+                 { type: "doc", label: "Expression Language", id: "articles/test-workflows-examples-expressions" },
+               //  { type: "doc", label: "Recipes", id: "articles/test-workflows-creating" },
+                 { type: "doc", label: "Variables and Secrets", id: "articles/adding-tests-secrets" },
+                 { type: "doc", label: "Scheduling", id: "articles/scheduling-tests" },
+               //  { type: "doc", label: "Migrating to Workflows", id: "articles/test-workflows-creating" },
+              ],
+            },
+            {
+              type: "category",
+              label: "Workflow Templates",
+              items: [
+                { type: "doc", label: "Overview", id: "articles/test-workflows-templates" },
+                { type: "doc", label: "Defining", id: "articles/test-workflows" },
+                { type: "doc", label: "Using", id: "articles/test-workflows-examples-template" },
+              //  { type: "doc", label: "Official Templates", id: "" },
+              ],
+            },
+            {
+              type: "category",
+              label: "Logs & Artifacts",
+              items: [
+                { type: "doc", label: "Log Collection", id: "articles/logging" },
+                { type: "doc", label: "Highlighting & Comparing", id: "testkube-pro/articles/log-highlighting" },
+                { type: "doc", label: "AI Analysis", id: "testkube-pro/articles/AI-test-insights" },
+                { type: "doc", label: "Artifacts", id: "articles/artifacts" },
+              ],
+            },
+            {
+              type: "category",
+              label: "Test Insights",
+              items: [
+                { type: "doc", label: "Operational Insights", id: "testkube-pro/articles/AI-test-insights" },
+                { type: "doc", label: "Quality Metrics", id: "/articles/metrics" },
+                { type: "doc", label: "Status Pages", id: "testkube-pro/articles/status-pages" },
+              ],
+            },
+            {
+              type: "category",
+              label: "Triggers",
+              items: [
+                { type: "doc", label: "Test Triggers", id: "articles/test-triggers" },
+                // { type: "doc", label: "K8 Eventss", id: "" },
+                // { type: "doc", label: "Testkube Events", id: "" },
+              ],
+            },
+            { type: "doc", label: "Webhooks", id: "articles/webhooks" },
+            {
+              type: "category",
+              label: "Testkube CLI",
+              items: [
+                { type: "doc", label: "Install the Testkube CLI", id: "articles/install/cli" },
+                { type: "doc", label: "Install Testkube with the CLI", id: "/articles/install/install-with-cli" },
+                { type: "doc", label: "Managing CLI Context", id: "testkube-pro/articles/managing-cli-context" },
+                { type: "doc", label: "Docker CLI", id: "articles/testkube-cli-docker" },
+                { type: "doc", label: "OAuth for CLI", id: "articles/oauth-cli" },
+              ],
+            },
+            {
+              type: "category",
+              label: "Advanced Features",
+              items: [
+                { type: "doc", label: "Cached Test Results", id: "testkube-pro/articles/cached-results" },
+                { type: "doc", label: "Templates", id: "/articles/templates" },
+                { type: "doc", label: "CDEvents", id: "articles/cd-events" },
+                { type: "doc", label: "Generating Test CRDs", id: "articles/generate-test-crds" },
+              ],
+            },
+            // { type: "doc", label: "Deprecated Features", id: "" },
+          ]  
+    },
+    {
+      type: "category",
+      label: "Administration",
+      items: [
+        { type: "doc", label: "Organizations", id: "testkube-pro/articles/organization-management" },
+        { type: "doc", label: "Environments", id: "testkube-pro/articles/environment-management" },
+        {
+          type: "category",
+          label: "User Management & RBAC",
+          items: [
+            { type: "doc", label: "Organization Members", id: "testkube-pro/articles/organization-management/#members" },
+            { type: "doc", label: "Environment Members", id: "testkube-pro/articles/environment-management/#managing-environment-member-roles" },
+            { type: "doc", label: "API Tokens", id: "testkube-pro/articles/organization-management/api-tokens" },
           ],
         },
-        "articles/adding-tests-secrets",
-        "articles/scheduling-tests",
-        "articles/test-triggers",
-        "articles/webhooks",
-        "articles/test-sources",
-        "articles/test-executions",
-        "articles/templates",
+        {
+          type: "category",
+          label: "SSO",
+          items: [
+           // { type: "doc", label: "Default User Mapping", id: "" },
+            { type: "doc", label: "OAuth for CLI", id: "/articles/oauth-cli" },
+          ],
+        },
       ],
     },
     {
       type: "category",
-      label: "Guides",
+      label: "Examples & Use Cases",
       items: [
         {
           type: "category",
-          label: "Getting to Production",
+          label: "Load Testing",
           items: [
-            {
-              type: "category",
-              label: "Authentication",
-              items: ["articles/oauth-cli"],
-            },
-            "articles/deploying-in-aws",
-            "articles/deploying-from-private-registries",
+            { type: "doc", label: "k6", id: "test-types/executor-k6" },
+            { type: "doc", label: "JMeter", id: "test-types/executor-jmeter" },
+            { type: "doc", label: "Artillery", id: "test-types/executor-artillery" },
+            { type: "doc", label: "Parallelization for Massive Load", id: "articles/test-workflows-examples-basics" },
           ],
         },
         {
           type: "category",
-          label: "CI/CD Integration",
-          link: {
-            type: "doc",
-            id: "articles/cicd-overview",
-          },
+          label: "E2E/FE Testing",
           items: [
-            {
-              type: "category",
-              label: "Github Actions",
-              link: {
-                type: "doc",
-                id: "articles/github-actions",
-              },
-              items: [
-                {
-                  type: "doc",
-                  id: "articles/run-tests-with-github-actions",
-                  label: "Migrate from testkube-run-action",
-                },
-              ],
-            },
-            "articles/gitlab",
-            "articles/jenkins",
-            "articles/jenkins-ui",
-            "articles/azure",
-            "articles/circleci",
-            "articles/testkube-cli-docker",
-            {
-              type: "category",
-              label: "GitOps",
-              link: {
-                type: "doc",
-                id: "articles/gitops-overview",
-              },
-              items: [
-                {
-                  type: "doc",
-                  id: "articles/flux-integration",
-                  label: "Flux",
-                },
-                {
-                  type: "doc",
-                  id: "articles/argocd-integration",
-                  label: "ArgoCD",
-                },
-              ],
-            },
+            { type: "doc", label: "Cypress", id: "test-types/executor-cypress" },
+            { type: "doc", label: "Playwright", id: "test-types/executor-playwright" },
+            // { type: "doc", label: "Robot Framework", id: "" },
+            // { type: "doc", label: "Selenium", id: "" },
+            { type: "doc", label: "Parallelized Execution", id: "articles/test-workflows-examples-basics" },
           ],
         },
-        "articles/creating-first-test",
-        "articles/cd-events",
-        "articles/k8s-events",
-        "articles/slack-integration",
-        "articles/generate-test-crds",
-        "articles/logging",
-        "articles/install-cli",
-        "articles/uninstall",
-        "articles/upgrade",
+        {
+          type: "category",
+          label: "API Testing",
+          items: [
+            { type: "doc", label: "Postman", id: "test-types/executor-postman" },
+            { type: "doc", label: "SoapUI", id: "test-types/executor-soapui" },
+          ],
+        },
+        // { type: "doc", label: "Unit Testing", id: "" },
+        {
+          type: "category",
+          label: "Automation Framework",
+          items: [
+            { type: "doc", label: "Maven", id: "test-types/executor-maven" },
+            { type: "doc", label: "Gradle", id: "test-types/executor-gradle" },
+          ],
+        },
+        {
+          type: "category",
+          label: "Compliance Testing",
+          items: [
+            { type: "doc", label: "Kubent", id: "test-types/executor-kubent" },
+          ],
+        },
       ],
     },
     {
       type: "category",
-      label: "Test Types",
-      link: {
-        type: "generated-index",
-        description: "Supported Test Types / Executors within Testkube",
-      },
+      label: "Integrations",
       items: [
-        "test-types/executor-artillery",
-        "test-types/executor-curl",
-        "test-types/executor-cypress",
-        "test-types/executor-ginkgo",
-        "test-types/executor-gradle",
-        "test-types/executor-jmeter",
-        "test-types/executor-k6",
-        "test-types/executor-kubepug",
-        "test-types/executor-kubent",
-        "test-types/executor-maven",
-        "test-types/executor-playwright",
-        "test-types/executor-postman",
-        "test-types/executor-pytest",
-        "test-types/executor-soapui",
-        "test-types/executor-tracetest",
-        "test-types/executor-zap",
-        "test-types/prebuilt-executor",
-        "test-types/container-executor",
-        "test-types/executor-distributed-jmeter",
+        { type: "doc", label: "GitHub", id: "articles/github-actions" },
+        { type: "doc", label: "Jenkins", id: "articles/jenkins" },
+        { type: "doc", label: "Azure DevOps", id: "articles/azure" },
+        { type: "doc", label: "GitLab", id: "articles/gitlab" },
+        // { type: "doc", label: "Tekton", id: "" },
+        { type: "doc", label: "ArgoCD", id: "articles/argocd-integration" },
+        { type: "doc", label: "Flux", id: "articles/flux-integration" },
+        // { type: "doc", label: "Argo Rollouts", id: "" },
+        // { type: "doc", label: "Flagger", id: "" },
+        { type: "doc", label: "CDEvents", id: "articles/cdevents" },
+        { type: "doc", label: "Slack Integration", id: "articles/slack-integration" },
+        { type: "doc", label: "Prometheus Metrics", id: "articles/metrics" },
       ],
     },
-    {
-      type: "html",
-      value: "<hr />",
-    },
-    {
-      type: "category",
-      label: "Testkube Pro",
-      items: [
-        "testkube-pro/articles/intro",
-        "testkube-pro/articles/installing-agent",
-        "testkube-pro/articles/transition-from-oss",
-        "testkube-pro/articles/organization-management",
-        "testkube-pro/articles/environment-management",
-        "testkube-pro/articles/managing-cli-context",
-        "testkube-pro/articles/architecture",
-        "testkube-pro/articles/running-parallel-tests-with-test-suite",
-        "testkube-pro/articles/AI-test-insights",
-        "testkube-pro/articles/status-pages",
-        "testkube-pro/articles/cached-results",
-        "testkube-pro/articles/log-highlighting",
-      ],
-    },
-    {
-      type: "category",
-      label: "Testkube Pro On-Prem",
-      items: [
-        "testkube-pro-on-prem/articles/testkube-pro-on-prem",
-        "testkube-pro-on-prem/articles/usage-guide",
-        "testkube-pro-on-prem/articles/auth",
-        "testkube-pro-on-prem/articles/migrating-from-oss-to-pro",
-      ],
-    },
-    "articles/testkube-oss",
     {
       type: "category",
       label: "Reference",
       items: [
+        "articles/architecture",
+        {
+          type: "category",
+          label: "CRDs",
+          items: [
+            { type: "doc", label: "TestWorkflow", id: "articles/test-workflows" },
+            { type: "doc", label: "Trigger", id: "articles/crd-reference" },
+          ],
+        },
         {
           type: "doc",
           id: "articles/helm-chart",
           label: "Helm Chart",
         },
-        "articles/crds-reference",
+        {
+          type: "category",
+          label: "OpenAPI Definition",
+          items: [
+            { type: "doc", label: "OSS", id: "articles/openapi" },
+            { type: "doc", label: "PRO", id: "articles/openapi" },
+          ],
+        },
         {
           type: "category",
           label: "CLI",
@@ -268,21 +301,16 @@ const sidebars = {
             },
           ],
         },
-        "openapi",
-        "articles/metrics",
-        "articles/artifacts",
-        "articles/testkube-dependencies",
-        "articles/architecture",
-        "articles/telemetry",
-        {
-          type: 'link',
-          label: 'OpenAPI Specification',
-          href: 'https://api.testkube.io/openapi',
-        },
       ],
     },
-    "articles/common-issues",
-    "articles/deprecations",
+    "articles/telemetry",
+
+    {
+      type: "html",
+      value: "<hr />",
+    },
+    
+    "articles/testkube-oss",
     {
       type: "category",
       label: "Contributing",
@@ -305,6 +333,13 @@ const sidebars = {
       items: ["articles/testkube-licensing-FAQ"],
     },
   ],
+
+  /*
+   {
+      type: "html",
+      value: "<hr />",
+    },
+   */
 
   // But you can create a sidebar manually
   /*
