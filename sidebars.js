7cd14fc40fab8d399f42c3eced2cc332513b5d2f
Dawid Rusnak <rangoo94@gmail.com>
2024-06-05 14:06:39 +0200

    feat: add docs for passing test content to the Test Workflows (#13)
    * feat: add docs for passing test content to the Test Workflows
    
    * Apply suggestions from code review
    
    Co-authored-by: Julianne Fermi <julianne@kubeshop.io>
    
    * chore: adjust the docs for expressions with new variables
    
    ---------
    
    Co-authored-by: Julianne Fermi <julianne@kubeshop.io>

--- sidebars.js
+++ sidebars.js
@@ -73,6 +73,7 @@ const sidebars = {
             "articles/test-workflow-templates",
             "articles/test-workflows-examples-basics",
             "articles/test-workflows-examples-configuration",
+            "articles/test-workflows-content",
             "articles/test-workflows-expressions",
             "articles/test-workflows-examples-templates",
             "articles/test-workflows-test-suites",
