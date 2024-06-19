193a8a76c07dcdd3b9cb780ed75ead147243d12c
Emil Davtyan <emil@kubeshop.io>
2024-06-19 14:49:09 +0200

    docs: vault docs & migrating istio docs

--- sidebars.js
+++ sidebars.js
@@ -433,6 +433,7 @@ const sidebars = {
         {type: "doc", label: "ArgoCD", id: "articles/argocd-integration"},
         {type: "doc", label: "Flux", id: "articles/flux-integration"},
         {type: "doc", label: "Vault", id: "articles/vault"},
+        {type: "doc", label: "Istio", id: "articles/istio"},
         {type: "doc", label: "CDEvents", id: "articles/cd-events"},
         {
           type: "doc",
