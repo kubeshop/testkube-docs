f120dc39d699c76af891fe299c76958e7a252b04
Bogdan Hanea <bogdan.hanea@yahoo.com>
2024-11-12 15:09:03 +0200

    signed commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/kubeshop/testkube/tree/develop/docs",
+          // Generating BreadCrumbs for the docs
           breadcrumbs: false,
         },
         blog: false,
